a9fa0a5324e4eae348da57eaf79d0a3ed1c2d14c
dylanmpeck <dylanmpeck@gmail.com>
2020-02-05 17:30:36 -0800

    Moving computers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] List<playerScript_ex00> characterControllers;
+
+    int currentCharacter = 1;
+
+    [SerializeField] string nextLevel = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChooseCharacter(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraFollow();
+
+        CheckIfPlayersAreAtExits();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChooseCharacter(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChooseCharacter(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChooseCharacter(3);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void ChooseCharacter(int charNum)
+    {
+        characterControllers[currentCharacter - 1].enabled = false;
+        currentCharacter = charNum;
+        characterControllers[currentCharacter - 1].enabled = true;
+    }
+
+    void CameraFollow()
+    {
+        mainCamera.transform.position = new Vector3(characterControllers[currentCharacter - 1].transform.position.x,
+                                            characterControllers[currentCharacter - 1].transform.position.y,
+                                            mainCamera.transform.position.z);
+    }
+
+    void CheckIfPlayersAreAtExits()
+    {
+        foreach (playerScript_ex00 controller in characterControllers)
+        {
+            if (controller.atExit == false)
+                return;
+        }
+        Debug.Log("All players have reached their exit!");
+
+        if (nextLevel != "")
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
---
+++ Assets/Scripts/playerScript_ex00.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class playerScript_ex00 : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField] float forceAmount;
+    [SerializeField] float jumpForce;
+
+    enum CharacterColor {Red, Yellow, Blue };
+
+    [SerializeField] CharacterColor myColor;
+
+    bool canJump = true;
+
+    [HideInInspector]
+    public bool atExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(Vector2.right * forceAmount);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Vector2.left * forceAmount);
+        }
+        if (canJump && Input.GetKey(KeyCode.Space))
+        {
+            canJump = false;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+            canJump = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == myColor.ToString())
+        {
+            atExit = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == myColor.ToString())
+        {
+            atExit = false;
+        }
+    }
+}
