b6c65d81ef0f607c26642b8f5e8771932d096660
Dylan Peck <dylanmpeck@gmail.com>
2020-02-07 15:35:41 -0800

    First draft for Itch.io. The first 3 levels are done. Needs polish and more complex level design

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    Vector3 startPos;
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] float distance = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = startPos + new Vector3(Mathf.Sin(speed * Time.time) * distance, 0, 0);
+    }
+}
--- Assets/Scripts/playerScript_ex00.cs
+++ Assets/Scripts/playerScript_ex00.cs
@@ -20,15 +20,21 @@ public class playerScript_ex00 : MonoBehaviour
     [HideInInspector]
     public bool atExit;
 
+    float distToGround;
+
+    [SerializeField] LayerMask lm;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        distToGround = GetComponent<BoxCollider2D>().bounds.extents.y + 0.1f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        CheckIfGrounded();
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.AddForce(Vector2.right * forceAmount);
@@ -44,17 +50,44 @@ public class playerScript_ex00 : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void CheckIfGrounded()
     {
-        if (collision.collider.tag == "Ground")
-            canJump = true;
+        if (rb.velocity.y <= 0.0f)
+        {
+            //int layerMask = 1 << LayerMask.NameToLayer("Ground") | 1 << LayerMask.NameToLayer("YellowPlayer") | 1 << LayerMask.NameToLayer("BluePlayer") | 1 << LayerMask.NameToLayer("RedPlayer");
+            //layerMask = layerMask & ~(1 << LayerMask.NameToLayer(myColor.ToString() + "Player"));
+            RaycastHit2D hit2D = Physics2D.Raycast(transform.position, Vector2.down, distToGround, lm);
+            if (hit2D)
+            {
+                canJump = true;
+            }
+        }
     }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.collider.tag == "GroundSurface" ||
+    //        collision.collider.tag == "Player")
+    //        canJump = true;
+    //}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == myColor.ToString())
         {
             atExit = true;
+        }
+        if (collision.tag == "Teleporter")
+        {
+            this.transform.position = collision.GetComponent<Teleporter>().destination.transform.position;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "MovingPlatform")
+        {
+            this.transform.SetParent(collision.transform);
         }
     }
 
@@ -63,6 +96,10 @@ public class playerScript_ex00 : MonoBehaviour
         if (collision.tag == myColor.ToString())
         {
             atExit = false;
+        }
+        if (collision.tag == "MovingPlatform")
+        {
+            this.transform.SetParent(null);
         }
     }
 }
